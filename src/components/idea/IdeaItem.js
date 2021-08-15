7e5d92c52b063adb4befa356561ee17f716cd182
Abdullah <abdallah.b.b96@gmail.com>
2021-08-15 19:12:06 +0300

    styling

--- src/components/idea/IdeaItem.js
+++ src/components/idea/IdeaItem.js
@@ -3,34 +3,38 @@ import { IdeaDiv, Button, L } from "../../style";
 
 const IdeaItem = (props) => {
   const idea = props.idea;
-
+  const ideaProgress=(idea.recievedFund/idea.fundAmount)*100
+console.log(ideaProgress)
   return (
     <>
       <div class="col-sm-3">
         <IdeaDiv class="card">
           <img src={idea.ideaPicture} class="card-img-top" alt={idea.name} />
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
+            <h5 class="card-title">{idea.ideaName}</h5>
+            <>
+            <img src="https://img.icons8.com/office/16/000000/money--v1.png"/>
+                                    <address>{idea.recievedFund}$ of {idea.fundAmount}$ </address>
             <div class="progress">
               <div
                 class="progress-bar progress-bar-striped bg-success"
                 role="progressbar"
-                style={{ width: "25%" }}
-                aria-valuenow="25"
+                style={{ width: `${ideaProgress}%` }}
+                aria-valuenow={idea.recievedFund}
                 aria-valuemin="0"
-                aria-valuemax="100"
+                aria-valuemax={idea.fundAmount}
               >
-                50%
-              </div>
             </div>
+            </div>
+            </>
             <p class="card-text">
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
+            {idea.ideaDescription}
             </p>
             <p class="card-text">
-              <small class="text-muted">Last updated 3 mins ago</small>
+              <small class="text-muted">{idea.createdAt.slice(0, 10)}</small>
             </p>
+
+
             <L to={`/ideas/${idea.slug}`}>
               <Button>More detials about my idea</Button>
             </L>
