3cfa2a74da2b701baa2c57f13cea47d28a8159b9
Abdullah <abdallah.b.b96@gmail.com>
2021-08-19 19:39:25 +0300

    done

--- src/style.js
+++ src/style.js
@@ -2,92 +2,138 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 export const Button = styled.button`
-  background-color: #7ae582;
-  margin-left: 4px;
-  color: #004e64;
+	background-color: #7ae582;
+	margin-left: 4px;
+	color: #004e64;
 `;
 export const L = styled(Link)`
-  margin-left: 4px;
+	margin-left: 4px;
 `;
 export const HomeDiv = styled.div`
-  margin-top: 50px;
+	margin-top: 50px;
 `;
 export const FDiv = styled.div`
-  background-color: #00a5cf;
+	background-color: #00a5cf;
 `;
 export const Image = styled.img`
-  height: 600px;
-  padding-left: 50px;
-  padding-right: 50px;
-  border-radius: 90px;
+	height: 600px;
+	padding-left: 50px;
+	padding-right: 50px;
+	border-radius: 90px;
 `;
 
 export const H2 = styled.h2`
-  font-family: cursive;
-  text-align: center;
-  margin-top: 35px;
-  color: #004e64;
+	font-family: cursive;
+	text-align: center;
+	margin-top: 35px;
+	color: #004e64;
 `;
 
 export const P = styled.p`
-  font-family: cursive;
-  text-align: center;
-  color: #004e64;
-  margin-left: 150px;
-  margin-right: 150px;
+	font-family: cursive;
+	text-align: center;
+	color: #004e64;
+	margin-left: 150px;
+	margin-right: 150px;
 `;
 
 export const IdeaDiv = styled.div`
-  border-style: solid;
-  border-width: 1px;
-  border-color: gray;
-  margin-top: 50px;
-  margin-left: 25px;
-  margin-right: 25px;
+	border-style: solid;
+	border-width: 1px;
+	border-color: gray;
+	margin-top: 50px;
+	margin-left: 25px;
+	margin-right: 25px;
 `;
 
 export const DetailDiv = styled.div`
-  border-style: solid;
-  border-width: 1px;
-  border-color: gray;
-  margin-left: 170px;
+	border-style: solid;
+	border-width: 1px;
+	border-color: gray;
+	margin-left: 170px;
 `;
 export const Form = styled.form`
-  margin-right: 400px;
-  margin-left: 400px;
-  margin-top: 100px;
-  margin-bottom: 300px;
+	margin-right: 400px;
+	margin-left: 400px;
+	margin-top: 100px;
+	margin-bottom: 300px;
 `;
 export const lisImage = styled.img`
-  height: 600px;
+	height: 600px;
 `;
 export const TextArea = styled.textarea`
-  margin-left: 600px;
-  width: 300px;
+	margin-left: 600px;
+	width: 300px;
 `;
 export const SendButton = styled.button`
-  width: 200px;
-  margin-left: 650px;
-  margin-top: 0;
+	width: 200px;
+	margin-left: 650px;
+	margin-top: 0;
 `;
 export const ChatImage = styled.img`
-  float: left;
-  max-width: 60px;
-  width: 100%;
-  margin-right: 20px;
-  border-radius: 50%;
+	float: left;
+	max-width: 60px;
+	width: 100%;
+	margin-right: 20px;
+	border-radius: 50%;
 `;
 export const ChatImageR = styled.img`
-  float: right;
-  max-width: 60px;
-  width: 100%;
-  margin-left: 20px;
-  margin-right: 0;
-  border-radius: 50%;
+	float: right;
+	max-width: 60px;
+	width: 100%;
+	margin-left: 20px;
+	margin-right: 0;
+	border-radius: 50%;
 `;
 
 export const Embed = styled.embed`
-  width: 700px;
-  height: 900px;
-  margin-left: 300px;
+	width: 700px;
+	height: 900px;
+	margin-left: 300px;
+`;
+
+export const Forme = styled.div`
+	text-align: center;
+	width: 800px;
+	height: 1500px;
+	border-style: solid;
+	border-width: 1px;
+	margin-right: 400px;
+	margin-left: 400px;
+	margin-top: 100px;
+	margin-bottom: 300px;
+	padding-left: 50px;
+	padding-right: 50px;
+	padding-top: 50px;
+	border-color: gray;
+`;
+
+export const BabeForme = styled.div`
+	padding-left: 50px;
+	padding-right: 50px;
+	padding-top: 50px;
+	padding-bottom: 300px;
+	border-color: gray;
+`;
+
+export const ThanksDiv = styled.div`
+	text-align: center;
+	border-color: gray;
+	font-family: cursive;
+	font-size: 100px;
+	color: black;
+	text-align: center;
+`;
+
+export const ThanksDiv2 = styled.div`
+	font-family: cursive;
+	font-size: 25px;
+	color: black;
+	text-align: center;
+`;
+
+export const FooterL = styled(Link)`
+	margin-left: 4px;
+	text-decoration: unset;
+	color: black;
 `;
