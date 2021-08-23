59bf1c75923fc04c6afdcfb28d2d6909a6405df3
Abdullah <abdallah.b.b96@gmail.com>
2021-08-23 17:32:06 +0300

    edit

--- src/components/idea/AdminIdeaItem.js
+++ src/components/idea/AdminIdeaItem.js
@@ -1,59 +1,73 @@
 import { useSelector } from "react-redux";
 
 //styling
-import { IdeaDiv, Button, L } from "../../style";
+import { ButtonCard, ListImage, L } from "../../style";
 
 const AdminIdeaItem = (props) => {
-  const idea = props.idea;
+	const idea = props.idea;
 
-  const ideaProgress = (idea.recievedFund / idea.fundAmount) * 100;
-  const users = useSelector((state) => state.users.users);
-  let user = users.find((user) => user.id === idea.ownerId);
+	const ideaProgress = (idea.recievedFund / idea.fundAmount) * 100;
+	const users = useSelector((state) => state.users.users);
+	let user = users.find((user) => user.id === idea.ownerId);
 
-  return (
-    <>
-      <div class="col-sm-3">
-        <IdeaDiv class="card">
-          <img src={idea.ideaPicture} class="card-img-top" alt={idea.name} />
-          <div class="card-body">
-            <h5 class="card-title">{idea.ideaName}</h5>
-            <>
-              <img
-                src="https://img.icons8.com/office/16/000000/money--v1.png"
-                alt="money"
-              />
-              <address>
-                {idea.recievedFund}$ of {idea.fundAmount}${" "}
-              </address>
-              <div class="progress">
-                <div
-                  class="progress-bar progress-bar-striped bg-success"
-                  role="progressbar"
-                  style={{ width: `${ideaProgress}%` }}
-                  aria-valuenow={idea.recievedFund}
-                  aria-valuemin="0"
-                  aria-valuemax={idea.fundAmount}
-                >
-                  {parseInt(ideaProgress)}%
-                </div>
-              </div>
-            </>
-            <p class="card-text">{idea.ideaDescription}</p>
-            <p class="card-text">
-              <small class="text-muted">
-                Idea created at {idea.createdAt.slice(0, 10)} by{" "}
-                {user?.firstName} {user?.lastName}
-              </small>
-            </p>
+	return (
+		<>
+			<div class="col-sm-4">
+				<div class="profile-card-4 text-center">
+					<ListImage src={idea.ideaPicture} class="img img-responsive" />
+					<div class="profile-content">
+						<div class="profile-name">{idea.ideaName}</div>
+						<div class="profile-description">
+							<h5>Fund Typy</h5>
+							{idea.fundType === "all" && (
+								<p>Donation , Investment or Consultation</p>
+							)}
+							{idea.fundType === "donation" && <p>Donation</p>}
+							{idea.fundType === "investmen" && <p>Investment</p>}
+							{idea.fundType === "consultation" && <p>Consultation</p>}
+							<img
+								src="https://img.icons8.com/office/16/000000/money--v1.png"
+								alt="money"
+							/>
+							<address>
+								{idea.recievedFund}$ of {idea.fundAmount}${" "}
+							</address>
+						</div>
+						<div class="row">
+							<div class="col-xs-4">
+								<div class="progress">
+									<div
+										class="progress-bar progress-bar-striped bg-success"
+										role="progressbar"
+										style={{ width: `${ideaProgress}%` }}
+										aria-valuenow={idea.recievedFund}
+										aria-valuemin="0"
+										aria-valuemax={idea.fundAmount}
+									>
+										{parseInt(ideaProgress)}%
+									</div>
+								</div>
+							</div>
 
-            <L to={`/adminlist/${idea.slug}`}>
-              <Button>More detials about my idea</Button>
-            </L>
-          </div>
-        </IdeaDiv>
-      </div>
-    </>
-  );
+							<div class="col-xs-4">
+								<div class="profile-overview">
+									<small class="text-muted">
+										Idea created at {idea.createdAt.slice(0, 10)} by{" "}
+										<b>
+											{user?.firstName} {user?.lastName}
+										</b>
+									</small>
+									<L to={`/adminlist/${idea.slug}`}>
+										<ButtonCard>More detials about my idea</ButtonCard>
+									</L>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default AdminIdeaItem;
