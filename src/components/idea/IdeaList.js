4298a336b5faaa082b33a6ea943ae18e8891267a
Abdullah <abdallah.b.b96@gmail.com>
2021-08-23 07:44:19 +0300

    list

--- src/components/idea/IdeaList.js
+++ src/components/idea/IdeaList.js
@@ -2,36 +2,20 @@ import { useSelector } from "react-redux";
 //components
 import IdeaItem from "./IdeaItem";
 import { IdeaDiv } from "../../style";
+import "./Ideas.css";
 const IdeaList = () => {
-  const ideas = useSelector((state) => state.ideas.ideas);
-  let ideaArray = ideas
-    .filter((_idea) => _idea.status === true)
-    .map((idea) => <IdeaItem idea={idea} key={idea?.id} />);
+	const ideas = useSelector((state) => state.ideas.ideas);
+	let ideaArray = ideas
+		.filter((_idea) => _idea.status === true)
+		.map((idea) => <IdeaItem idea={idea} key={idea?.id} />);
 
-  return (
-    <>
-      <div>
-        <div class="row">{ideaArray}</div>
-      </div>
-
-      {/* <div class="card shadow-xl image-full">
-        <figure>
-          <img src="https://picsum.photos/id/1005/400/250" />
-        </figure>
-        <div class="justify-end card-body">
-          <h2 class="card-title">Image overlay</h2>
-          <p>
-            Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit
-            sit necessitatibus veritatis sed molestiae voluptates incidunt iure
-            sapiente.
-          </p>
-          <div class="card-actions">
-            <button class="btn btn-primary">Get Started</button>
-          </div>
-        </div>
-      </div> */}
-    </>
-  );
+	return (
+		<>
+			<div>
+				<div class="row">{ideaArray}</div>
+			</div>
+		</>
+	);
 };
 
 export default IdeaList;
